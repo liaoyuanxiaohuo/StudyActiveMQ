50029c24e1d299c0529e7decde30a115e57499bd
Administrator <Administrator@XUAM8XVNZN27W0G>
2017-04-26 11:29:31 +0800

    Yes

---
+++ ActiveMQRecive/Program.cs
@@ -0,0 +1,65 @@
+﻿using Apache.NMS;
+using Apache.NMS.ActiveMQ;
+using Apache.NMS.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActiveMQRecive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                #region 消息消费者
+                string queuesName = "myQueue";
+                Uri _uri = new Uri(String.Concat("activemq:tcp://localhost:61616?wireFormat.maxInactivityDuration=0"));
+                IConnectionFactory factory = new ConnectionFactory(_uri);
+                // IConnectionFactory factory = new ConnectionFactory();
+                using (IConnection conn = factory.CreateConnection("admin", "manager"))
+                {
+                    using (ISession session = conn.CreateSession())
+                    {
+                        conn.Start();
+                        IDestination destination = SessionUtil.GetDestination(session, queuesName);
+                        using (IMessageConsumer consumer = session.CreateConsumer(destination))
+                        {
+                            consumer.Listener += (IMessage message) =>
+                            {
+                                ITextMessage msg = (ITextMessage)message;
+                                Console.WriteLine("接收消息：" + msg.Text);
+                            };
+                            // consumer.Listener += new MessageListener(consumer_Listener);
+                            Console.ReadLine();
+                        }
+                    }
+                }
+                #endregion
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+
+        static void consumer_Listener(IMessage message)
+        {
+            try
+            {
+                ITextMessage msg = (ITextMessage)message;
+                Console.WriteLine("接收：" + msg.Text);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ ActiveMQSend/Program.cs
@@ -0,0 +1,44 @@
+﻿using Apache.NMS;
+using Apache.NMS.ActiveMQ;
+using Apache.NMS.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ActiveMQSend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string queuesName = "myQueue";
+            Uri _uri = new Uri(String.Concat("activemq:tcp://localhost:61616"));
+            IConnectionFactory factory = new ConnectionFactory(_uri);
+            //IConnectionFactory factory = new ConnectionFactory();
+            using (IConnection conn = factory.CreateConnection("admin", "manager"))
+            {
+                using (ISession session = conn.CreateSession())
+                {
+                    IDestination destination = SessionUtil.GetDestination(session, queuesName);
+                    using (IMessageProducer producer = session.CreateProducer(destination))
+                    {
+                        conn.Start();
+                        //可以写入字符串，也可以是一个xml字符串等
+                        for (int i = 0; i < 100; i++)
+                        {
+                            ITextMessage request = session.CreateTextMessage("新新messsage" + i);
+                            producer.Send(request);
+                            Console.WriteLine("发送新新消息：" + i);
+                            Thread.Sleep(1000);
+
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+}
